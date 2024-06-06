4a3aa0830abdb62d55b5f71fdd2c86cf789f3dba
Andrei <dy-repay>
2024-06-06 18:23:03 +0800

    update for post service

--- lib/features/post/services/post.service.dart
+++ lib/features/post/services/post.service.dart
@@ -1,22 +1,26 @@
-
-
 import 'package:dio/dio.dart';
 import 'package:probi_flutter/common/env.dart';
+import 'package:probi_flutter/features/post/models/post.dart';
 
 class PostApi {
   final dio = Dio();
 
-  Future<void> getPosts() async {
-    try{
+  Future<List<Post>> getPosts() async {
+    try {
       final response = await dio.get(baseUrl);
 
-      if(response.statusCode == 200){
-        
+      if (response.statusCode == 200) {
+        List<dynamic> data = response.data as List<dynamic>;
+        List<Post> posts = data
+            .map((json) => Post.fromJson(json as Map<String, dynamic>))
+            .toList();
+
+        return posts;
+      } else {
+        throw Exception('Failed to load posts');
       }
-    } catch (e){
+    } catch (e) {
       throw Exception('Failed to find posts: $e');
     }
-
   }
-
 }
