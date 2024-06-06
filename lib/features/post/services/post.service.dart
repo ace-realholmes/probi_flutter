4f6e5b35b02283218cd6d31f889d0db548682647
Andrei <dy-repay>
2024-06-06 18:13:34 +0800

    Utilize freeze for post model

---
+++ lib/features/post/services/post.service.dart
@@ -0,0 +1,25 @@
+
+
+import 'package:dio/dio.dart';
+import 'package:probi_flutter/common/env.dart';
+
+class PostApi {
+  final dio = Dio();
+
+  Future<void> getPosts() async {
+    try{
+      final response = await dio.get(baseUrl);
+
+      if(response.statusCode == 200){
+        
+      }
+
+
+
+    } catch (e){
+      throw Exception('Failed to find posts: $e');
+    }
+
+  }
+
+}
