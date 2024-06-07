bc1f089899e34d1da6eeda9335acfe824273d1b6
Andrei <dy-repay>
2024-06-07 10:24:42 +0800

    add post services

--- lib/features/post/services/post.service.dart
+++ lib/features/post/services/post.service.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:dio/dio.dart';
 import 'package:probi_flutter/common/env.dart';
 import 'package:probi_flutter/features/post/models/post.dart';
@@ -5,7 +7,7 @@ import 'package:probi_flutter/features/post/models/post.dart';
 class PostApi {
   final dio = Dio();
 
-  Future<List<Post>> getPosts() async {
+  Future<List<Post>> getAllPosts() async {
     try {
       final response = await dio.get(baseUrl);
 
@@ -21,6 +23,74 @@ class PostApi {
       }
     } catch (e) {
       throw Exception('Failed to find posts: $e');
+    }
+  }
+
+  Future<Post> getPost(int id) async{
+    try{
+      final response = await dio.get("$baseUrl/$id");
+
+      if(response.statusCode == 200){
+        Post post = Post.fromJson(response.data as Map<String, dynamic>);
+        print('Get Post: ${post.body}');
+        return post;
+        
+      } else{
+        throw Exception('Failed to Get Post: ${response.statusMessage}');
+      }
+
+    } catch(e){
+      throw Exception('Failed to Get Post: $e');
+    }
+  }
+
+  Future<Post> putPost(int id) async {
+    try{
+      final response = await dio.put("$baseUrl/$id");
+
+      if(response.statusCode == 200){
+        Post post = Post.fromJson(response.data as Map<String, dynamic>);
+        print("Put Post: $post");
+        return post;
+      }else {
+        throw Exception("Failed to Put Post: ${response.statusMessage}");
+      }
+
+    } catch(e){
+      throw Exception("Failed to Put Post: $e");
+    }
+  }
+
+  Future<Post> patchPost(int id) async{
+    try{
+      final response = await dio.patch("$baseUrl/$id");
+
+      if(response.statusCode == 200){
+        Post post = Post.fromJson(response.data as Map<String, dynamic>);
+        print("Patch post: ${post.body}");
+        return post;
+      } else {
+        throw Exception("Failed to patch post: ${response.statusMessage}");
+      }
+
+    } catch(e){
+      throw Exception("Failed to patch post: $e");
+    }
+  }
+
+  Future<void> deletePost(int id) async{
+    try{
+      final response = await dio.delete("$baseUrl/$id");
+
+      if(response.statusCode == 200) {
+        print("Post successfully deleted");
+      } else{
+        print("Post failed to delete");
+        throw Exception("Failed to delete post: ${response.statusMessage}");
+      }
+
+    } catch(e){
+      throw Exception("Failed to delete post: $e");
     }
   }
 }
